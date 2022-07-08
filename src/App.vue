2f9adb50a346bed6a0c48a7f91660094b9f94d99
Rob Manfred <volnovakha@protonmail.com>
2022-07-08 08:08:01 +0000

    refactor: modify a landing page

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img 
+    class="main__logo"
+    alt="MLB logo" 
+    src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png"
+  />
+  <HelloWorld msg="Welcome to MLB App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.main__logo {
+  max-width: 40rem
+}
+</style>
